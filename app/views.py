cc166884daf1f26b921e8c5f7077b58a5fe9c26a
Sreenivasulu-Gattu <sreenivasgattu12345@gmail.com>
2023-11-20 19:11:29 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def sree(request):
+    return render(request,'sree.html')
+
+def gf(request):
+    return render(request,'gf.html')
+
+def sree2(request):
+    return render(request,'sree2.html')
